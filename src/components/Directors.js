d03957aeb53793bd44dcfffcd29888b21bcdd40c
marisasaunders <marisaksaunders@gmail.com>
2022-05-11 18:34:07 -0400

    all tests passed WOOOO

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorItems = directors.map((director) => {
+    return (
+      <div key={director.name}>
+        <h2>Name: {director.name}</h2>
+        <ul>Movies:
+          {director.movies.map((movie) => (
+            <li key={movie}>{movie}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {directorItems}
+  </div>
+  )
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieItems = movies.map((movie) => {
+    return (
+      <div key={movie.title}>
+        <h2>Name: {movie.title}</h2>
+        <p>Time: {movie.time}</p>
+        <ul>Genres:
+          {movie.genres.map((genre) => (
+            <li key={genre}>{genre}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    {movieItems}
+  </div>
+  )
 }
 
 export default Movies;
